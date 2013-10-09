6bcedb4fbf2d05547bc58898d2566f2353a7aed1
Ryan Hurst <rhurst@lm127337-rhurs.jomax.paholdings.com>
2013-10-09 13:35:44 -0700

    More Cleanup

--- django_blog/apps/contact/urls.py
+++ django_blog/apps/contact/urls.py
@@ -5,27 +5,6 @@ from contact.views import *
 # admin.autodiscover()
 
 urlpatterns = patterns('contact.views',
-	#url(r'^$', landing),
 	url(r'^contact/$', contact),
-	#url(r'^articles/(?P<article_slug>[a-z-0-9]+)/$', article),
-	#url(r'^about/$', about_page),
-	
-	#url(r'^login/$', user_login),
-	#url(r'^logout/$', logout),
 
-	
-	#url(r'^register/$', register),
-	#url(r'^forgot_password/$', forgot_password),
-	#url(r'^settings/$', settings),
-	
-
-	# Examples:
-	# url(r'^$', 'mylifing.views.home', name='home'),
-	# url(r'^mylifing/', include('mylifing.foo.urls')),
-
-	# Uncomment the admin/doc line below to enable admin documentation:
-	# url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-	# Uncomment the next line to enable the admin:
-	# url(r'^admin/', include(admin.site.urls)),
 )
